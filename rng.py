cf6785bb8f06b935b60053ca9cefe3b110d0e5c5
Nova <99283269+NovasGreat@users.noreply.github.com>
2025-02-06 18:23:53 +0000

    getting set up for adding recipes

--- rng.py
+++ rng.py
@@ -16,6 +16,7 @@ os_info = platform.uname()
 roll = "none"
 oneoutof = "none"
 
+
 def os_check():
     if platform.system() == "Windows":
         os.system('cls')
@@ -38,8 +39,8 @@ lapiz_Lazuli_count = 0
 quartz_count = 0
 coal_count = 0
 copper_count= 0
-iron_count = 0
-gold_count = 0
+iron_block_count = 0
+gold_block_count = 0
 furnace_count = 0
 stonecutter_count = 0
 chest_count = 0
@@ -64,8 +65,8 @@ def item_check():
     print(f"Crafting Table count: {crafting_table_count}")
     print(f"Coal count: {coal_count}")
     print(f"Copper count: {copper_count}")
-    print(f"Iron count: {iron_count}")
-    print(f"Gold count: {gold_count}") 
+    print(f"Iron Block count: {iron_block_count}")
+    print(f"Gold Block count: {gold_block_count}") 
     print(f"Furnace count: {furnace_count}")
     print(f"Stonecutter count: {stonecutter_count}")
     print(f"Chest count: {chest_count}")
@@ -76,9 +77,27 @@ def item_check():
     print(f"Emerald count: {emerald_count}")
 
 
+# crafting recipes, potions (50 pulls each):
+# 1/2 Luck Potion (100 Dirt, 32 Glass) (Shop Cooldown: none)
+# 1/4 Luck Potion (64 Oak Logs (or 256 Oak Planks), 32 Glass) (Shop Cooldown: none)
+# 1/8 Luck Potion (64 Wool, 64 Oak Logs 32 Glass) (Shop Cooldown: 100 Pulls)
+# 1/16 Luck Potion (32 Copper, 64 Oak Logs 32 Glass) (Shop Cooldown: 500 Pulls)
+# 1/32 Luck Potion (32 Amethyst, 64 Oak Logs, 32 Glass) (Shop Cooldown: 1000 Pulls)
+# 1/64 Luck Potion (40 Diamond, 32 Glass) (Shop Cooldown: 2500 Pulls) 
 
+# Wooden Pickaxe (20 Durability, 5 Oak Logs)
+# Wooden Axe (20 Durability, 5 Oak Logs)
+# Stone Axe (100 Durability, 5 Cobblestone, 5 Sticks)
+# Stone Pickaxe (100 Durability, 5 Cobblestone, 5 Sticks)
+# Iron Pickaxe (750 Durability, 10 Iron, 10 Sticks)
+# Iron Axe (750 Durability, 10 Iron, 10 Sticks)
+# Gold Pickaxe (500 Durability, 10 Gold, 10 Sticks)
+# Gold Axe (500 Durability, 10 Gold, 10 Sticks)
+# Diamond Pickaxe (1000 Durability, 30 Diamond, 64 Sticks)
+# Diamond Axe (1000 Durability, 30 Diamond, 64 Sticks)
 
-
+# Oak Planks x4 (1 Oak Log, 1 Axe Hit)
+# Sticks x4 (1 Oak Plank, 4 Axe Hits)
 
 
 
@@ -98,15 +117,16 @@ print(probability)
 
 #----------------------
 two = np.array([
-'Dirt'
+'Dirt',
+'Cobblestone'
 
 ])
 four = np.array([
 "Oak Log"
 
+
 ])
-eight = np.array([
-"Cobblestone",
+eight = np.array()
 "Wool",
 "Snow",
 "Lapiz Lazuli",
@@ -116,8 +136,8 @@ eight = np.array([
 ])
 sixteen = np.array([
 "Copper",
-"Iron",
-"Gold"
+"Iron Block",
+"Gold Block"
 
 ])
 thirtytwo = np.array([
@@ -147,8 +167,8 @@ increment_map = {
     "Quartz":"quartz_count",
     "Coal":"coal_count",
     "Copper":"copper_count",
-    "Iron":"iron_count" ,
-    "Gold":"gold_count" ,
+    "Iron Block":"iron_block_count" ,
+    "Gold Block":"gold_block_count" ,
     "Furnace":"furnace_count",
     "Stonecutter":"stonecutter_count",
     "Chest":"chest_count",
